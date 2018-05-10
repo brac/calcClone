7e365d3ef8752b5903c35d5262c574a3fb3edd7d
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-05-10 12:00:42 -0500

    clear button

--- index.js
+++ index.js
@@ -2,6 +2,21 @@
 // TODO: Disable drag effect on keys
 // TODO: Add Flash when key typed
 
+// TODO: Function Keys
+  // /
+  // x
+  // -
+  // +
+  // =
+
+// TODO: Adjust Keys
+  // +/-
+  // %
+
+// TODO: Compute math equaltion
+  // Store value when currentNumber is computational value
+
+
 (function() {
   let keypad = '';
   let outputText = '';
@@ -9,22 +24,20 @@
   let fontSize = '';
 
   function add(number) {
-    return `You passed ${number} to be added`;
+    // TODO: Seperate current total and new number, return new total
+    // currentTotal = parseInt(currentNumber);
+    // number = parseInt(number);
+    // let newTotal = currentTotal + number;
+    // return newTotal;
+
+    return "A computation will be preformed";
   }
 
-  function calculator(key) {
+  function updateOutputView(string) {
+    outputText.textContent = string;
+  }
 
-    // Preform computations
-    if (key != parseInt(key) ) {
-      switch (key){
-        case '+':
-          console.log(add(currentNumber));
-      }
-    } else {
-      currentNumber += key;
-    }
-
-
+  function checkOutputViewTextSize(){
     // Decrease font size as number increases length
     if (fontSize == '') {
       fontSize = '3';
@@ -37,38 +50,42 @@
         outputText.style.fontSize = `${fontSize}em`;
       }
     }
+    // TODO: Make the font size go back up if the numbers reduce
+  }
 
-    // Print the new number to the output view
-    outputText.textContent = currentNumber;
-
-        // TODO: Function Keys
-      // /
-      // x
-      // -
-      // +
-      // =
-
-    // TODO: Adjust Keys
-      // AC
-      // +/-
-      // %
-
-    // TODO: Compute math equaltion
-      // Store value when currentNumber is computational value
+  function calculator(key) {
+    // Determine if function key or number key
+    if (isNaN(parseInt(key))) {
+      // TODO: Do some computations based on key pressed
+      switch (key){
+        case 'a':
+        case'AC':
+          updateOutputView('0');
+          currentNumber = '';
+          break;
+        default:
+          console.log(add(currentNumber));
+          break;
+      }
+    } else {
+      // Add the last key pressed to the display
+      currentNumber += key;
+      checkOutputViewTextSize();
+      updateOutputView(currentNumber);
+    }
   }
 
   document.addEventListener('DOMContentLoaded', function() {
     keypad = document.querySelector('.calculator-keypad');
     outputText = document.querySelector('.calculator-outputview-output');
-    fontSize = outputText.style.fontSize;
 
     keypad.addEventListener('click', function(event){
       calculator(event.target.textContent);
     });
 
+// TODO: Make the font size go back up if the numbers reduce
     document.addEventListener('keypress', function(event) {
       if (/[0-9]|\/|\*|\-|\+|Enter|a|=/.test(event.key)) {
-        console.log('huzzah!');
         calculator(event.key);
       }
     });
