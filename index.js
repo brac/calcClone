ad71211baa614ce79017c49633326eae4524928e
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-05-10 13:03:10 -0500

    basic addition

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // TODO: Add JSDoc annotations
 // TODO: Disable drag effect on keys
 // TODO: Add Flash when key typed
@@ -20,17 +21,60 @@
 (function() {
   let keypad = '';
   let outputText = '';
-  let currentNumber = '';
   let fontSize = '';
+  let oldNumber = '';
+  let resultNumber = '';
+  let currentNumber = '';
 
-  function add(number) {
+  document.addEventListener('DOMContentLoaded', function() {
+    keypad = document.querySelector('.calculator-keypad');
+    outputText = document.querySelector('.calculator-outputview-output');
+
+    keypad.addEventListener('click', function(event){
+      calculator(event.target.textContent);
+    });
+
+    document.addEventListener('keypress', function(event) {
+      if (/[0-9]/.test(event.key)) {
+        calculator(event.key);
+      } else if (/\/|\*|\-|\+|Enter|a|=/.test(event.key)) {
+
+        switch (event.key) {
+          case 'a':
+            oldNumber = '';
+            updateOutputView('0');
+            break;
+
+          case '+':
+            if (oldNumber == '') {
+              oldNumber = currentNumber;
+              currentNumber = '';
+              break;
+            }
+
+
+            resultNumber = add(oldNumber, currentNumber);
+            console.log({oldNumber, currentNumber, resultNumber});
+
+            oldNumber = resultNumber;
+            currentNumber = '';
+            updateOutputView(resultNumber);
+        }
+      }
+    });
+  });
+
+  function calculator(key) {
+    // Add the last key pressed to the display
+    currentNumber += key;
+    checkOutputViewTextSize();
+    updateOutputView(currentNumber);
+  }
+
+  function add(oldNumber, currentNumber) {
     // TODO: Seperate current total and new number, return new total
-    // currentTotal = parseInt(currentNumber);
-    // number = parseInt(number);
-    // let newTotal = currentTotal + number;
-    // return newTotal;
-
-    return "A computation will be preformed";
+    resultNumber = parseInt(oldNumber) + parseInt(currentNumber);
+    return resultNumber;
   }
 
   function updateOutputView(string) {
@@ -50,44 +94,5 @@
         outputText.style.fontSize = `${fontSize}em`;
       }
     }
-    // TODO: Make the font size go back up if the numbers reduce
   }
-
-  function calculator(key) {
-    // Determine if function key or number key
-    if (isNaN(parseInt(key))) {
-      // TODO: Do some computations based on key pressed
-      switch (key){
-        case 'a':
-        case'AC':
-          updateOutputView('0');
-          currentNumber = '';
-          break;
-        default:
-          console.log(add(currentNumber));
-          break;
-      }
-    } else {
-      // Add the last key pressed to the display
-      currentNumber += key;
-      checkOutputViewTextSize();
-      updateOutputView(currentNumber);
-    }
-  }
-
-  document.addEventListener('DOMContentLoaded', function() {
-    keypad = document.querySelector('.calculator-keypad');
-    outputText = document.querySelector('.calculator-outputview-output');
-
-    keypad.addEventListener('click', function(event){
-      calculator(event.target.textContent);
-    });
-
-// TODO: Make the font size go back up if the numbers reduce
-    document.addEventListener('keypress', function(event) {
-      if (/[0-9]|\/|\*|\-|\+|Enter|a|=/.test(event.key)) {
-        calculator(event.key);
-      }
-    });
-  });
 })();
